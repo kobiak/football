1e2d73905efefd9fe62671e601ac0283e6b4b023
Evgeny Kobyakov <kobiak@icqz.net>
2016-09-26 14:48:20 +1300

    update app.js fix cookies

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     });
 
     var loadLeagues = true;
-    var loadLegaueTable = true;
+    var loadLegueTable = true;
 
     //function getCookie        
     function getCookie(cname) {
@@ -26,102 +26,117 @@ $(document).ready(function () {
     };
 
     //get cookie 
-    var cookiename = getCookie("load-leagues");
-    if (cookiename == "false") {
-        loadLeagues = false
+    var cookienameLeagues = getCookie("load-leagues");
+    if (cookienameLeagues == "false") {
+        loadLeagues = false        
     };
 
-    var cookiename = getCookie("load-table-" + dataTablePref);
-    if (cookiename == "false") {
-        loadLegaueTable = false
+    var $appBox = $('.app-box');
+    var $introScreen = '<section class="intro-screen"><h1>Football Results</h1><p>Please select league:</p></section>';
+    var $leagueScreen = '<section class="league-screen"><div class="select-league"></div></section>';
+    var $tableData = '<section class="table-data"><div class="table"></div></section>';
+    var $tableHeader = '<div class="table-header"><div></div><div>P</div><div>W</div><div>D</div><div>L</div><div>GD</div><div>Pts</div></div>';
+
+    function tableScreen() {
+        // load leagues
+
+        if ((localStorage.getItem('Leagues') !== null) && (!loadLeagues)) {
+
+            $('.league-name').html('');
+
+            // insert into text and leagues list
+            $appBox.html($introScreen + $leagueScreen);
+
+            console.log('use localstorage - table screen');
+
+            // retrive response from localstorage
+            var appData = JSON.parse(localStorage.getItem('Leagues'));
+
+            $.each(appData, function (index) {
+                var name = appData[index].caption;
+                var link = appData[index]._links.leagueTable.href;
+                var leagueName = appData[index].league;
+
+                $('.select-league').append('<a href="#" data-table-url="' + link + '" data-table-pref="' + leagueName + '">' + name + '</a>');
+
+            });
+
+
+        } else {
+            $.ajax({
+                headers: {
+                    'X-Auth-Token': 'ad36d94a3eaf4dcf802b05c867b5e9e6'
+                },
+                url: 'https://api.football-data.org/v1/competitions',
+                dataType: 'json',
+                type: 'GET',
+                success: function (response) {
+
+                    $('.league-name').html('');
+
+                    // insert into text and leagues list
+                    $appBox.html($introScreen + $leagueScreen);
+
+                    // save response to localstorage
+                    var appData = JSON.stringify(response);
+                    localStorage.setItem('Leagues', appData);
+                    var appData = JSON.parse(localStorage.getItem('Leagues'));
+
+                    console.log('use ajax - table screen');
+
+                    $.each(appData, function (index) {
+                        var name = appData[index].caption;
+                        var link = appData[index]._links.leagueTable.href;
+                        var leagueName = appData[index].league;
+
+                        $('.select-league').append('<a href="#" data-table-url="' + link + '" data-table-pref="' + leagueName + '">' + name + '</a>');
+
+                    });
+
+                    var date = new Date();
+                    date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+                    date.toGMTString();
+
+                    document.cookie = "load-leagues=false;  path=/; expires=" + date + ";"
+                },
+                beforeSend: function () {
+
+                    $('.app-box').addClass('overlay');
+                    $('.app-box').append('<div class="overlay-box"><div class="loader"><svg viewBox="0 0 32 32" width="32" height="32"><circle id="spinner" cx="16" cy="16" r="14" fill="none"></circle></svg></div></div>')
+
+                },
+                complete: function () {
+
+                    $('.app-box').removeClass('overlay');
+                    $('.overlay-box').remove();
+                },
+                error: function () {
+                    $('.app-box').hide();
+                    $('body').append('<section class="offline"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i><h3>API server is offline :(</h3></section>');
+                }
+
+            });
+        };
     };
 
-    // load leagues
 
-    if ((localStorage.getItem('Leagues') !== null) && (!loadLeagues)) {
-
-        console.log('use localstorage');
-
-        // retrive response from localstorage
-        var appData = JSON.parse(localStorage.getItem('Leagues'));
-
-        $.each(appData, function (index) {
-            var name = appData[index].caption;
-            var link = appData[index]._links.leagueTable.href;
-            var leagueName = appData[index].league;
-
-            $('.select-league').append('<a href="#" data-table-url="' + link + '" data-table-pref="' + leagueName + '">' + name + '</a>');
-
-        });
-
-
-    } else {
-        $.ajax({
-            headers: {
-                'X-Auth-Token': 'ad36d94a3eaf4dcf802b05c867b5e9e6'
-            },
-            url: 'https://api.football-data.org/v1/competitions',
-            dataType: 'json',
-            type: 'GET',
-            success: function (response) {
-
-                // save response to localstorage
-                var appData = JSON.stringify(response);
-                localStorage.setItem('Leagues', appData);
-                var appData = JSON.parse(localStorage.getItem('Leagues'));
-
-                console.log('use ajax');
-
-                $.each(appData, function (index) {
-                    var name = appData[index].caption;
-                    var link = appData[index]._links.leagueTable.href;
-                    var leagueName = appData[index].league;
-
-                    $('.select-league').append('<a href="#" data-table-url="' + link + '" data-table-pref="' + leagueName + '">' + name + '</a>');
-
-                });
-
-                var date = new Date();
-                date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
-                date.toGMTString();
-
-                document.cookie = "load-leagues=false;  path=/; expires=" + date + ";"
-            },
-            beforeSend: function () {
-
-                $('.home-screen').addClass('overlay');
-                $('.home-screen').append('<div class="overlay-box"><div class="loader"><svg viewBox="0 0 32 32" width="32" height="32"><circle id="spinner" cx="16" cy="16" r="14" fill="none"></circle></svg></div></div>')
-
-
-            },
-            complete: function () {
-
-                $('.home-screen').removeClass('overlay');
-                $('.overlay-box').remove();
-            },
-            error: function () {
-                $('.home-screen').hide();
-                $('body').append('<section class="offline"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i><h3>API server is offline :(</h3></section>');
-            }
-
-        });
-    };
 
     var dataTableUrl = '',
         dataTablePref = '';
 
-    $('.table-data').hide();
-
-    function hideHomeScreen() {
-        $('.home-screen').hide();
-    };
-
     function showTableData() {
-        $('.table-data').show();
 
-        if ((localStorage.getItem(dataTablePref) !== null) && (!loadLegaueTable)) {
+        var cookienameLeagueTable = getCookie("load-table-" + dataTablePref);
 
-            console.log('use localstorage');
+        if (cookienameLeagueTable == "false") {
+            loadLegueTable = false           
+        };
+
+        $appBox.html($tableData);
+
+        if ((localStorage.getItem(dataTablePref) !== null) && (!loadLegueTable)) {
+
+            console.log('use localstorage - build table');
 
             buildTable();
 
@@ -134,7 +149,7 @@ $(document).ready(function () {
     $('body').on('click touch', '.select-league a', function () {
         dataTableUrl = $(this).data('table-url');
         dataTablePref = $(this).data('table-pref');
-        hideHomeScreen();
+        
         showTableData();
     });
 
@@ -151,11 +166,11 @@ $(document).ready(function () {
         showHideMenu()
     });
 
-    $('.home-button').on('click touch', function (e) {
+    $('.table-button').on('click touch', function (e) {
         e.preventDefault();
-        $('.league-name').html('');
-        $('.home-screen').show();
-        $('.home-screen').next('section').hide();
+
+        tableScreen();
+
         showHideMenu();
         $('.update-table').css('display', 'none');
 
@@ -210,7 +225,7 @@ $(document).ready(function () {
                 var appData = JSON.stringify(response);
                 localStorage.setItem(dataTablePref, appData);
 
-                console.log('use ajax');
+                console.log('use ajax - build table');
 
                 buildTable();
 
